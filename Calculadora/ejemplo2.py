f4c5a3e0cf063ddef9051948608d11b94f34218a
Axel García Triviño <agarciatrivino07@gmail.com>
2026-03-11 18:17:05 +0100

    actualizando

--- Calculadora/ejemplo2.py
+++ Calculadora/ejemplo2.py
@@ -2,64 +2,71 @@
 """
 (1) suma
 (2) resta
-(3) multi
-(4) division
-(5) raiz cuadrada
+(3) multiplicación
+(4) división
+(5) raíz cuadrada
 """
 
-print ('Bienvenido a la calculadora de Axel, espero que cumpla su función' .center(50,'-'))
-print(' Opcion 1 Suma',('\n'),'Opcion 2 Resta',('\n'),'Opcion 3 Multiplicacion',('\n'),'Opcion 4 Division',('\n'),'Opcion 5 Raiz cuadrada')
+print(f'Bienvenido a la calculadora de Axel, espero que cumpla su función' .center(60,'-'))
+print(f'Opcion 1 Suma',('\n'),'Opcion 2 Resta',('\n'),'Opcion 3 Multiplicación',('\n'),'Opcion 4 Division',('\n'),'Opcion 5 Raiz cuadrada')
 
 variable1 = int(input('Primer valor''\n'))
 variable2 = int(input('Segundo valor''\n'))
-
 
 def calculadora (num1, num2, opcion):
 
     opcion = int(input("Elige una opcion" '\n'))
     if opcion == 1:
         print(f'El resultado de la suma de {num1} + {num2}: {num1 + num2}')
+        continuar = input("¿Quieres seguir utilizando el servicio? (si/no)")
+        if continuar != "si":
+            print(" Fin del programa")
+            return 0
+        else:
+            print(f'Introduce los valores')
+            calculadora(num1, num2,opcion)
+
     elif opcion == 2:
-            print(f'El resultado de la resta de {num1} - {num2}: {num1 - num2}')
+        print(f'El resultado de la resta de {num1} - {num2}: {num1 - num2}')
+        continuar = input("¿Quieres seguir utilizando el servicio? (si/no)")
+        if continuar != "si":
+            print(" Fin del programa")
+            return 0
+        else:
+            print(f'Introduce los valores')
+            calculadora(num1, num2,opcion)    
     elif opcion == 3:
-        print(f'El resultado de la multiplicacion de {num1} * {num2}: {num1 * num2}')  
+        print(f'El resultado de la multiplicacion de {num1} * {num2}: {num1 * num2}')
+        continuar = input("¿Quieres seguir utilizando el servicio? (si/no)")
+        if continuar != "si":
+            print(" Fin del programa")
+            return 0
+        else:
+            print(f'Introduce los valores')
+            calculadora(num1, num2,opcion)
 
     elif opcion == 5:
         print(f'El resultado de la raiz cuadrada es {num1} y {num1} : {num1 / 2}')
-
     elif opcion == 4: 
         if num2 == 0:
             print("Error: No se puede dividir  entre cero. ")
         else: 
             print(f"El resultado de la division es: {num1 / num2}")
             continuar = input("¿Quieres realizar otra operacion (si/no):").lower()
-
+            
         if continuar != "si":
             print("Fin del programa")
-            return 0; 
+            return 0
     
         si = True
         no = False
         
         if si:
             calculadora(num1, num2, opcion)
-        
-
-        """else:
-            while True:
-                num2 = input("Pon un numero")
-                # Solicitamos la entrada al usuario
-                if num2.isdigit():
-                    num2 = int(opcion)
-                    print(f"Numero invalido: {opcion}")
-                    break
-            else:
-                print("Entrada invalida, solo admite números")
-        """
-
     else: 
-        print('Te equivocaste de opcion')
+        print('Te equivocaste de opción')
+        print(f'Inserta otra vez la opción deseada')
+        calculadora(num1, num2,opcion)
 
 calculadora(variable1,variable2,2,)
-
-print('Finalizada la operacion, gracias por utilizar mi servicio')
+print('Finalizada la operación, gracias por utilizar mi servicio')
